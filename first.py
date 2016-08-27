098db8cc778289c228c1bfeee390a3f7811aab24
Parth Mehta <parthm548@gmail.com>
2016-08-27 16:16:23 +0530

    implement model in admin

---
+++ first.py
@@ -0,0 +1,44 @@
+"""def print_name(a):
+	result='Welcome' + a
+	return result
+
+name=input()
+res=print_name(name)
+print(res)
+
+# name = input()
+for i in range(0,10):
+	print(i)
+	n=input()
+
+#array
+
+l=[1,2,3]
+l.insert(1,5)
+l.append(4)
+
+l.pop(1)
+print(l)
+
+l=[1,2,3,4,5,6,7,8,9,10]
+for i in l:
+	print(i**2)
+
+t = 10
+if t==12:
+	print(12)
+elif t==9:
+	print(9)
+else:
+	print(10)"""
+
+d = {'name':'parth', 'year':2}
+print(d['name'])
+print(d.keys())
+print(d.values())
+print(d)
+
+
+
+
+
